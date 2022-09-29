fb4af71e5c8d78b1eee924016c828ec2c43e8a90
holipah23 <holipah2310@gmail.com>
2022-09-29 15:15:55 +0700

    menambahkan folder wp2_19210352

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"
+ Nama saya Holipah Nuraisah <br>
+ Saya tinggal di bersama orang tua <br>
+ olah raga yang saya sukai sewaktu saya SMP adalah
+ futsal
+ ";
+ }
+}

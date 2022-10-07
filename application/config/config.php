8968741324a8141382457527bc0cad89b170b694
holipah23 <holipah2310@gmail.com>
2022-10-07 10:45:32 +0700

    membuat templating website sederhana

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+//$config['base_url'] = 'http://localhost/wp2_19210352/';
+$config ['base url'] = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+$config ['base_url'] .= '://'. $_SERVER['HTTP_HOST'];
+$config ['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
